611e63dd0dcda936914b82509a47125ed039cf33
Gerry0303 <92528396+gurusingagerry03@users.noreply.github.com>
2025-06-26 00:18:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "https://web-production-f037.up.railway.app/predict"
 
 st.title("🏠 House Price Prediction")
 
-# User inputs
+# User Inputs
 area = st.number_input("Living Area (GrLivArea)", min_value=300, max_value=5000, value=1000)
 bedrooms = st.selectbox("Number of Bedrooms", [1, 2, 3, 4, 5])
 bathrooms = st.selectbox("Number of Full Bathrooms", [1, 2, 3])
@@ -27,6 +27,7 @@ if st.button("Predict Price"):
             st.success(f"💰 Predicted Price: ${result['predicted_price']:,}")
             st.info(f"Estimated Range: ${result['min_estimate']:,} - ${result['max_estimate']:,}")
         else:
-            st.error("Prediction failed. Please check your API.")
-    except:
-        st.error("Failed to connect to the API. Is it running?")
+            st.error(f"Prediction failed. Status: {res.status_code}")
+    except Exception as e:
+        st.error(f"Failed to connect to the API. Error: {e}")
+
